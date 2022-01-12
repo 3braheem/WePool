b7aebb405d03e331c327565465367c57de00581f
3braheem <ibraahimsiri@gmail.com>
2022-01-11 16:42:08 -0800

    Mappings and Pool structure

--- src/WePool.sol
+++ src/WePool.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.11;
 /// @author 3braheem
 contract WePool {
     struct Pool {
+        uint256 index;
         PoolMember admin;
         PoolMember[] regulars;
         uint256 balance;
@@ -19,8 +20,34 @@ contract WePool {
         Administrator,
         Regular
     }
-    mapping(address => Role) roleState;
-    mapping(address => bool) memberState;
+    mapping(uint256 => mapping(address => Role)) poolRoleState;
+    mapping(uint256 => mapping(address => bool)) poolMemberState;
+    mapping(address => uint256) poolGroup;
 
-    function appoint(address _admin) public onlyAdmin notNull {}
+    modifier onlyAdmin(uint256 _pool, address _claimant) {
+        require(poolRoleState[_pool][_claimant] == Role.Administrator);
+        _;
+    }
+
+    modifier notNull(address _input) {
+        require(_input != address(0));
+        _;
+    }
+
+    modifier isAMember(uint256 _pool, address _input) {
+        require(poolMemberState[_pool][_input] == true);
+        _;
+    }
+
+    function payout(address _receiver)
+        public
+        payable
+        onlyAdmin(poolGroup[msg.sender], msg.sender)
+        notNull(_receiver)
+        isAMember(poolGroup[msg.sender], _receiver)
+    {
+        require(poolGroup[msg.sender] != 0);
+        (bool success, ) = _receiver.call{value: msg.value}("");
+        require(success, "The call was unsuccessful.");
+    }
 }
