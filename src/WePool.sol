c54ac65c4a1b00b842e69436f593571dec656768
3braheem <ibraahimsiri@gmail.com>
2022-01-13 13:48:37 -0800

    Init functionality

--- src/WePool.sol
+++ src/WePool.sol
@@ -5,14 +5,14 @@ pragma solidity 0.8.11;
 /// @author 3braheem
 contract WePool {
     struct Pool {
-        uint256 index;
         address admin;
-        address[] regulars;
+        address[] members;
         uint256 balance;
     }
-    Pool[] pools;
+    Pool[] public pools;
 
     enum Role {
+        NA,
         Administrator,
         Regular
     }
@@ -35,19 +35,57 @@ contract WePool {
         _;
     }
 
-    event NewPoolCreated(address[] indexed members, uint256 indexed balance)
+    modifier poolExists(uint256 _poolIndex) {
+        require(_poolIndex < pools.length, "Not an existing pool.");
+        _;
+    }
 
-    function initGroup(address[] _members, uint256 _startingBalance)
+    event NewPoolCreated(
+        address[] indexed members,
+        uint256 indexed balance,
+        uint256 indexed id
+    );
+
+    event DepositMade(
+        uint256 indexed id,
+        uint256 indexed amount,
+        address indexed sender
+    );
+
+    event DepositReceived(uint256 indexed amount, address indexed sender);
+
+    receive() external payable {
+        emit DepositReceived(msg.value, msg.sender);
+    }
+
+    function initGroup(
+        address[] memory _members,
+        address _admin,
+        uint256 _startingBalance
+    ) public payable returns (uint256) {
+        uint256 poolIndex = pools.length;
+        pools.push(
+            Pool({admin: _admin, members: _members, balance: _startingBalance})
+        );
+        for (uint256 i = 0; i < _members.length; i++) {
+            poolGroup[_members[i]] = poolIndex;
+            poolMemberState[poolIndex][_members[i]] = true;
+            poolRoleState[poolIndex][_members[i]] = Role.Regular;
+        }
+        poolRoleState[poolIndex][_admin] = Role.Administrator;
+        emit NewPoolCreated(_members, _startingBalance, poolIndex);
+        return poolIndex;
+    }
+
+    function deposit(uint256 _poolIndex)
         public
         payable
+        isAMember(_poolIndex, msg.sender)
+        poolExists(_poolIndex)
     {
-        uint poolIndex = pools.length;
-        Pool storage pool = ({
-            index: poolIndex;
-            regulars: _members;
-            balance: _startingBalance;
-        })
-        emit NewPoolCreated(_members, balance);
+        (bool success, ) = address(this).call{value: msg.value}("");
+        require(success, "The call was unsuccessful.");
+        emit DepositMade(_poolIndex, msg.value, msg.sender);
     }
 
     function payout(address _receiver)
