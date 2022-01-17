721e364f1c43a5e3c27bcb21108423e7847e2506
3braheem <ibraahimsiri@gmail.com>
2022-01-16 17:39:29 -0800

    Admin functionality

--- src/WePool.sol
+++ src/WePool.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.11;
 
-/// @title A Fund Pooling project for community investment
-/// @author 3braheem
+/**  @title A Fund Pooling project for community investment
+     @author 3braheem
+**/
 contract WePool {
     struct Pool {
         address admin;
@@ -16,27 +17,53 @@ contract WePool {
         Administrator,
         Regular
     }
-    mapping(uint256 => mapping(address => Role)) poolRoleState;
-    mapping(uint256 => mapping(address => bool)) poolMemberState;
-    mapping(address => uint256) poolGroup;
+    mapping(uint256 => mapping(address => Role)) public poolRoleState;
+    mapping(uint256 => mapping(address => bool)) public poolMemberState;
+    mapping(uint256 => mapping(address => mapping(address => bool)))
+        public memberConsent;
+    mapping(address => uint256) internal poolGroup;
+    uint256 public threshold;
 
+    /** 
+        @notice                 Requires that a function be called solely by the admin of a specific pool
+        @param _pool            The pool which is being interacted with
+        @param _claimant       The address who is trying to access the function
+    **/
     modifier onlyAdmin(uint256 _pool, address _claimant) {
         require(poolRoleState[_pool][_claimant] == Role.Administrator);
         _;
     }
 
+    /** 
+        @notice                 Requires an address inputted into a function to not be the 0 address
+        @param _input           The address that is inputted into the function
+    **/
     modifier notNull(address _input) {
         require(_input != address(0));
         _;
     }
 
+    /** 
+        @notice                 Requires the specified address to be a member of a specified pool
+        @param _pool            The pool which is being interacted with
+        @param _input           The address that is inputted 
+    **/
     modifier isAMember(uint256 _pool, address _input) {
         require(poolMemberState[_pool][_input] == true);
         _;
     }
 
+    /** 
+        @notice                 Requires the inputted index to exist in the system
+        @param _poolIndex       The index of a pool in the pools array 
+    **/
     modifier poolExists(uint256 _poolIndex) {
         require(_poolIndex < pools.length, "Not an existing pool.");
+        _;
+    }
+
+    modifier consensusReached(uint256 _accepted, uint256 _length) {
+        require(_accepted == _length, "Consensus has not been reached");
         _;
     }
 
@@ -75,6 +102,32 @@ contract WePool {
         poolRoleState[poolIndex][_admin] = Role.Administrator;
         emit NewPoolCreated(_members, _startingBalance, poolIndex);
         return poolIndex;
+    }
+
+    function approveAdmin(uint256 _poolIndex, address _admin)
+        public
+        isAMember(_poolIndex, msg.sender)
+        isAMember(_poolIndex, _admin)
+        poolExists(_poolIndex)
+    {
+        memberConsent[_poolIndex][msg.sender][_admin] = true;
+    }
+
+    function appointAdmin(uint256 _poolIndex, address _admin)
+        public
+        isAMember(_poolIndex, msg.sender)
+        isAMember(_poolIndex, _admin)
+        poolExists(_poolIndex)
+    {
+        Pool storage pool = pools[_poolIndex];
+        address[] memory members = pool.members;
+        for (uint256 i = 0; i < members.length; i++) {
+            require(
+                memberConsent[_poolIndex][members[i]][_admin] = true,
+                "Consent has not been reached."
+            );
+        }
+        poolRoleState[_poolIndex][_admin] = Role.Administrator;
     }
 
     function deposit(uint256 _poolIndex)
